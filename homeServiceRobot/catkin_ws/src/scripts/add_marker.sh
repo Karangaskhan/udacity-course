fec9f5c628d51e1f37a7b0f8e46df2551bc7a24d
Karan Purohit <karan.purohit@crown.com>
2019-05-14 15:40:39 +1200

    Feat: Add most functionality for homeServiceRobot

---
+++ homeServiceRobot/catkin_ws/src/scripts/add_marker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/export/bulk/local-home/kpurohit/projects/udacity-course/homeServiceRobot/catkin_ws/src/map/karanWorld.world" &
+sleep 5
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/export/bulk/local-home/kpurohit/projects/udacity-course/homeServiceRobot/catkin_ws/src/map/karanWorldMap.yaml" &
+sleep 5
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e "rosrun add_markers add_markers"
